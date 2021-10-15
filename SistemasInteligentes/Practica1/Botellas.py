0c907cf60975e5eaeb94ec1cd43eb5235f60f36b
Carlos Pedraza Antona <74327838+Pedrazaax@users.noreply.github.com>
2021-10-15 11:33:27 +0200

    Práctica Laboratorio Sistemas Inteligentes

---
+++ SistemasInteligentes/Practica1/Botellas.py
@@ -0,0 +1,52 @@
+'''
+Created on 5 oct. 2021
+
+@author: Carlos
+'''
+
+import json
+from textwrap import indent
+
+
+"""JSON"""
+
+
+f = open('Estados.txt', 'r')
+content = f.read()
+objetoJson = json.dumps(content, indent=4)
+
+print(objetoJson)
+
+f = open('Estados.txt', 'w')
+f.write(objetoJson)
+f.close
+
+
+'''
+acciones = open('Acciones.txt', 'r')
+c = acciones.read()
+accionesJson = json.dumps(c)
+'''
+
+""" Leer fichero .txt 
+
+fichero = open('Estados.txt')
+
+print(fichero.read())
+
+"""
+
+'''JSON a Python
+
+datosJSON ='{"name": "Carlos", "edad":39}'
+objetoJson = json.loads(datosJSON)
+print(objetoJson)
+
+Python a JSON
+
+datosPython = {'name': 'Carlos', 'edad':44, 'isEmployed':True}
+diccionariotoJSON = json.dumps(datosPython)
+print(diccionariotoJSON)
+
+'''
+
