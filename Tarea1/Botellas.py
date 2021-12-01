0437fd3c9c15dd2e19b7f4695eb2b44b9e9d7d0b
Pedrazaax <carlos.pedraza@alu.uclm.es>
2021-12-01 19:37:49 +0100

    Añadido metodo Es_AccionPosible Añadido metodo total_cantidad Modificado y arreglado JSON

--- Tarea1/Botellas.py
+++ Tarea1/Botellas.py
@@ -6,11 +6,12 @@ Created on 5 oct. 2021
 
 import json
 
-print("Tarea 1 ")
+class Json:
+    def __init__(self,estado):
+        self.estado = estado
 
-vector_estados=[]
-   
 def leer_fichero(vector_estados):
+    
     with open("Estados.txt","r") as f:
         content = f.readlines()
         linea = 0
@@ -18,24 +19,62 @@ def leer_fichero(vector_estados):
             linea += 1
             try:
                 objson = json.loads(i)
-                vector_estados.append(f"Estado:{objson}")
-                print(f"Estado numero  {linea}:{objson}")
+                vector_estados.append(objson)
+                #vector_estados_json.append(Json(objson).__dict__)
                 
             except (json.decoder.JSONDecodeError, Exception) as e:
                     print(f"\n Error en el Estado nº  {linea}: {e}\n")
 
-def test1(vector_estados):
+def imprime_json(vector_estados):
+    print(json.dumps(vector_estados, indent=2))
+    
+def ES_AccionPosible (Botella_origen, Botella_destino, Cantidad):
+    max = 9
+    posible = False
+    if total_cantidad(Botella_origen) >= Cantidad:
+        if (total_cantidad(Botella_destino)+Cantidad) <= max:
+            posible = True
+        else:
+            posible = False
+
+    return posible
+
+def total_cantidad (Botella):
+    suma = 0
+    for i in range(0, len(Botella)):
+        suma = Botella[i][0]
+
+    return suma
+
+def escribir_json(vector_estados):
     with open('Estados.json', 'w') as f:
-        json.dump(vector_estados, f)
+        s=json.dumps(vector_estados, indent=2)
+        f=open("Estados.json","w")
+        f.write(s)
+        f.close()
         
-    print("\n")
-    print(vector_estados[0])
-    #print(vector_estados[0][1])
-    #print(vector_estados[0][1][0][0])
+def main():
+    print("Tarea 1 ")
+    
+    vector_estados=[]
+    vector_estados_json=[]    
+    leer_fichero(vector_estados)
     
     
-leer_fichero(vector_estados)
-test1(vector_estados)
+    imprime_json(vector_estados)
+    posible=ES_AccionPosible (vector_estados[4][0], vector_estados[4][1], 7)
+    
+    print(posible)
+    
+    escribir_json(vector_estados)
+
+
+try:
+    main()
+except KeyboardInterrupt:
+    print("exit")
+
+
 
 
 
