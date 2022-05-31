8bafd29ad899d0bcaeb2021b0fb7aff3c94de7b5
Alejandro Villamayor Zapero <91552023+ZaP3R0@users.noreply.github.com>
2022-05-31 12:08:46 +0200

    Add files via upload
    Clase Botella creada

---
+++ ProyectoLAB_SI/practicaSI/src/Botella.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+public class Botella {
+    private int idBotella;
+	private int cantidadMax;
+	private int cantidadOcupada;
+	private Stack<Color> pilaColores;
+
+    public Botella(int idBotella, int cantidadMax) {
+        this.idBotella = idBotella;
+		this.cantidadMax = cantidadMax;
+        pilaColores = new Stack<Color>();
+        cantidadOcupada = 0;
+    }
+
+    private Stack<Color> copiarPila (Stack<Color> pilaOrigen) {
+        Stack<Color> aux = new Stack<Color>();
+		Stack<Color> copia = new Stack<Color>();
+
+        while(!pilaOrigen.empty()) {
+            aux.add(pilaOrigen.pop()); 
+        }
+
+        while (!aux.empty()) {
+            copia.add(new Color(aux.peek().getId(), aux.peek().getCantidad()));
+            pilaOrigen.add(aux.pop());
+        }
+        return copia;
+    }
+    
+
+    public int getIdBotella() {
+		return idBotella;
+	}
+
+	public int getCantidadMax() {
+		return cantidadMax;
+	}
+
+	public int getCantidadOcupada() {
+		return cantidadOcupada;
+	}
+
+    
+}
